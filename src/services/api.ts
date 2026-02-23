574bdd3b2a3df4043e77c006630f9ce286b6f94f
Pedro <pedropaulo@interlinksistemas.com.br>
2026-02-23 15:01:04 -0300

    Chamada da API

---
+++ src/services/api.ts
@@ -0,0 +1,10 @@
+export const API_URL = "https://api.abracops.info";
+
+export async function apiFetch(endpoint: string, options?: RequestInit) {
+    const response = await fetch (`${API_URL}${endpoint}`, options);
+    if (!response.ok) {
+        const error = await response.json()
+        throw new Error(error.message || "Erro na requisição da API");
+    }
+    return response.json();
+}
