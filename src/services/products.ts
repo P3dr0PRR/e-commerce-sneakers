ca535fbaf1e3f35ea2accda9c80a5c5403cd3d56
Pedro <pedropaulo@interlinksistemas.com.br>
2026-02-23 15:35:46 -0300

    Definindo a aba products

---
+++ src/services/products.ts
@@ -0,0 +1,36 @@
+import { apiFetch } from "./api";
+
+// export async function createSneaker(
+//   name: string,
+//   price: number,
+//   token: string,
+// ) {
+//   return apiFetch("/products", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: `Bearer ${token}`,
+//     },
+//     body: JSON.stringify({ name, price }),
+//   });
+// }
+
+export async function getSneakers(token: string) {
+  return apiFetch("/products", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+}
+
+// export async function deleteSneakers(id: number, token: string) {
+//   return apiFetch(`/products/${id}`, {
+//     method: "DELETE",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: `Bearer ${token}`,
+//     },
+//   });
+// }
