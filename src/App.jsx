cb7993c4c8cc72600a6996f215acdc43ac0eec90
Pedro <pedropaulo@interlinksistemas.com.br>
2026-02-25 16:42:08 -0300

    feat: configurando rota inicial

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/admin" element={<Admin />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
